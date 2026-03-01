e73a6623def57080c6cbe01fc7e4c43b683f0a1d
Yuki <ktc1cristian@gmail.com>
2026-02-28 20:55:50 -0500

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,12 +1,31 @@
 <?php
 
+// return [
+// 'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'user'],
+// 'supports_credentials' => true,
+// 'allowed_origins' => [
+//     'http://localhost:5173',
+//     'http://127.0.0.1:5173',
+// ],
+// 'allowed_methods' => ['*'],
+// 'allowed_headers' => ['*'],
+// ];
+
+
+
 return [
-'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'user'],
-'supports_credentials' => true,
-'allowed_origins' => [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-],
-'allowed_methods' => ['*'],
-'allowed_headers' => ['*'],
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'supports_credentials' => true,
+
+    'allowed_origins' => [
+        'https://fualtec.com.ec',
+        'https://www.fualtec.com.ec',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_headers' => ['*'],
+
 ];
